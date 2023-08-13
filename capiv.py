894f8405fbb6217ffbd573994cf6dac39a04173f
Agustina Gilardone <142050237+aggg97@users.noreply.github.com>
2023-08-13 18:19:28 -0300

    Update capiv.py

--- capiv.py
+++ capiv.py
@@ -31,9 +31,7 @@ data_sorted['date'] = pd.to_datetime(data_sorted['anio'].astype(str) + '-' + dat
 # Convert the "date" column to datetime format
 data_sorted['date'] = pd.to_datetime(data_sorted['date'])
 
-st.header("Capítulo IV Dataset - Producción No Convencional",
-    style={"color": "blue"},
-)
+st.header(f":blue[Capítulo IV Dataset - Producción No Convencional]")
 image = Image.open('Vaca Muerta rig.png')
 st.sidebar.image(image)
 st.sidebar.title("Por favor filtrar aquí: ")
