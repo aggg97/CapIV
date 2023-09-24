a31ee0ee9f95333b46230e296f734893bd7cca57
Agustina Gilardone <142050237+aggg97@users.noreply.github.com>
2023-09-24 22:48:14 +0200

    Update 2_📊_Multi-well_Comparison.py

--- pages/2_📊_Multi-well_Comparison.py
+++ pages/2_📊_Multi-well_Comparison.py
@@ -3,11 +3,65 @@ import pandas as pd
 import plotly.graph_objects as go
 from PIL import Image
 
+COLUMNS=[
+    'sigla', #atemporal
+    'anio', #temporal
+    'mes',#temporal
+    'prod_pet',#temporal
+    'prod_gas',#temporal
+    'prod_agua',#temporal
+    'iny_gas',#temporal
+    'tef', #temporal
+    'tipoextraccion', #atemporal
+    'tipopozo',#atemporal
+    'empresa',#atemporal
+    'formacion', #atemporal
+    'areayacimiento',#atemporal
+    'fecha_data' #temporal
+    ]
+
+COLUMNS_NAMES=[
+    'Sigla',
+    'Año', 
+    'Mes',
+    'Producción de Petróleo (m3)',
+    'Producción de Gas (km3)',
+    'Producción de Agua (m3)',
+    'Inyección de Gas (km3)',
+    'TEF', 
+    'Tipo de Extracción', 
+    'Tipo de Pozo',
+    'Empresa',
+    'Formación', 
+    'Área yacimiento',
+    'Fecha de Datos' 
+    ]
+
+# Reorder and rename the columns in the DataFrame
 @st.cache(allow_output_mutation=True)
 def load_and_sort_data(dataset_url):
-    df = pd.read_csv(dataset_url)
-    data_sorted = df.sort_values(by='fecha_data', ascending=True)
+    df = pd.read_csv(dataset_url, usecols=COLUMNS)
+    # data_sorted = df.sort_values(by='fecha_data', ascending=True)
+    data_sorted=df.sort_values(by=['sigla', 'fecha_data'],ascending=True)
+    data_sorted = data_sorted[COLUMNS]
+    # Calculate gas rate for the filtered data
+    data_sorted['gas_rate'] = data_sorted['prod_gas'] / data_sorted['tef']
+
+    # Calculate oil rate and water rate for the filtered data
+    data_sorted['oil_rate'] = data_sorted['prod_pet'] / data_sorted['tef']
+    data_sorted['water_rate'] = data_sorted['prod_agua'] / data_sorted['tef']
+
+    # Add a new column "Np" with the cumulative sum of "prod_pet" for each 'sigla'
+    data_sorted['Np'] = data_sorted.groupby('sigla')['prod_pet'].cumsum()
+
+    # Add a new column "Gp" with the cumulative sum of "prod_gas" for each 'sigla'
+    data_sorted['Gp'] = data_sorted.groupby('sigla')['prod_gas'].cumsum()
+
+    # Add a new column "Wp" with the cumulative sum of "prod_agua" for each 'sigla'
+    data_sorted['Wp'] = data_sorted.groupby('sigla')['prod_agua'].cumsum
+    
     return data_sorted
+
 
 # URL of the dataset
 dataset_url = "http://datos.energia.gob.ar/dataset/c846e79c-026c-4040-897f-1ad3543b407c/resource/b5b58cdc-9e07-41f9-b392-fb9ec68b0725/download/produccin-de-pozos-de-gas-y-petrleo-no-convencional.csv"
@@ -20,138 +74,122 @@ data_sorted['date'] = pd.to_datetime(data_sorted['anio'].astype(str) + '-' + dat
 # Convert the "date" column to datetime format
 data_sorted['date'] = pd.to_datetime(data_sorted['date'])
 
+ # Create a Pivot Table to Calculate Maximum Oil and Gas Rates for Each Well
+pivot_table = data_sorted.pivot_table(
+    values=['gas_rate', 'oil_rate','water_rate'],
+    index=['sigla'],
+    aggfunc={'gas_rate': 'max', 'oil_rate': 'max', 'water_rate': 'max'}
+)
+
+# Step 2: Create a New DataFrame with Maximum Oil and Gas Rates
+max_rates_df = pivot_table.reset_index()
+max_rates_df['GOR'] = max_rates_df['gas_rate'] / max_rates_df['oil_rate']
+max_rates_df['GOR'] = max_rates_df['GOR'].fillna(100000) 
+
+# Add a new column "Fluido McCain" based on conditions
+max_rates_df['Fluido McCain'] = max_rates_df.apply(
+    lambda row: 'Gas' if row['oil_rate'] == 0 or row['GOR'] > 3000 else 'Petróleo',
+    axis=1 
+)
+
+
 st.header(f":blue[Capítulo IV Dataset - Producción No Convencional]")
 image = Image.open('Vaca Muerta rig.png')
 st.sidebar.image(image)
 st.sidebar.title("Por favor filtrar aquí: ")
 
-# Create a multiselect widget for 'tipo pozo'
-# soon... type fluid classification by GOR (McCain)
-tipos_pozo = data_sorted['tipopozo'].unique()
-selected_tipos_pozo = st.sidebar.multiselect("Seleccionar tipo de pozo:", tipos_pozo)
+# Create a dropdown list for "Fluido McCain"
+selected_fluido = st.sidebar.selectbox("Seleccionar tipo de fluido según McCain:", max_rates_df['Fluido McCain'].unique())
 
-# Create a dropdown list for 'empresa'
-empresas = data_sorted['empresa'].unique()
-selected_empresa = st.sidebar.selectbox("Seleccionar operadora:", empresas)
+# Create a multiselect list for 'sigla'
+selected_sigla = st.sidebar.multiselect("Seleccionar siglas de los pozos a comparar", max_rates_df['sigla'])
 
-# Filter data based on selected 'tipo pozo' and 'empresa'
-matching_data = data_sorted[
-    (data_sorted['tipopozo'].isin(selected_tipos_pozo)) &
-    (data_sorted['empresa'] == selected_empresa)
+# Filter data for matching 'sigla'
+filtered_data = data_sorted[
+    (data_sorted['sigla'].isin(selected_sigla))
 ]
 
-# Get unique 'sigla' values based on selected 'empresa' and 'tipo pozo'
-siglas_for_selected_empresa = matching_data['sigla'].unique()
-
-# Create a dropdown list for 'sigla'
-selected_sigla = st.sidebar.selectbox("Seleccionar sigla del pozo", siglas_for_selected_empresa)
-
-# Filter data for matching 'empresa' and 'sigla'
-matching_data = matching_data[matching_data['sigla'] == selected_sigla]
-
-
-# Filter data for matching 'empresa' and 'sigla'
-matching_data = data_sorted[
-    (data_sorted['empresa'] == selected_empresa) &
-    (data_sorted['sigla'] == selected_sigla)
-]
-
-# Display the filtered data table
-# soon... download to .xls 
-if st.button(f"Ver datos históricos del pozo: {selected_sigla}"):
-    st.write("Filtered Data:")
-    st.write(matching_data)
-
-# Calculate gas rate for the filtered data
-matching_data['gas_rate'] = matching_data['prod_gas'] / matching_data['tef']
-
-# Calculate oil rate and water rate for the filtered data
-matching_data['oil_rate'] = matching_data['prod_pet'] / matching_data['tef']
-matching_data['water_rate'] = matching_data['prod_agua'] / matching_data['tef']
-
-# Create a counter column for x-axis
-matching_data['counter'] = range(1, len(matching_data) + 1)
-
-# Filter data for matching 'tipo pozo'
-matching_tipo_pozo_data = data_sorted[data_sorted['tipopozo'].isin(selected_tipos_pozo)]
-
-# Calculate max peak rates for the selected_sigla
-max_gas_rate = matching_data['gas_rate'].max()
-max_oil_rate = matching_data['oil_rate'].max()
-max_water_rate = matching_data['water_rate'].max()
-
-# Round the maximum rates to one decimal place
-max_gas_rate_rounded = round(max_gas_rate, 1)
-max_oil_rate_rounded = round(max_oil_rate, 1)
-max_water_rate_rounded = round(max_water_rate, 1)
-
-st.header(selected_sigla)
-col1, col2, col3 = st.columns(3)
-col1.metric(label=f":red[Caudal Máximo de Gas (km3/d)]", value=max_gas_rate_rounded)
-col2.metric(label=f":green[Caudal Máximo de Petróleo (m3/d)]", value=max_oil_rate_rounded)
-col3.metric(label=f":blue[Caudal Máximo de Agua (m3/d)]", value=max_water_rate_rounded)
 
 # Plot gas rate using Plotly
 gas_rate_fig = go.Figure()
 
-gas_rate_fig.add_trace(
-    go.Scatter(
-        x=matching_data['counter'],
-        y=matching_data['gas_rate'],
-        mode='lines+markers',
-        name='Gas Rate',
-        line=dict(color='red')
+for sigla in selected_sigla:
+    filtered_well_data = filtered_data[filtered_data['sigla'] == sigla]
+    
+    # Filter data to start when 'Np' is different from zero
+    filtered_well_data = filtered_well_data[filtered_well_data['Np'] != 0]
+    
+    # Add a counter column to the filtered data
+    filtered_well_data['counter'] = range(1, len(filtered_well_data) + 1)
+    
+    gas_rate_fig.add_trace(
+        go.Scatter(
+            x=filtered_well_data['counter'],  # Use the counter as x-axis
+            y=filtered_well_data['gas_rate'],
+            mode='lines+markers',
+            name=f'Gas Rate - {sigla}'
+        )
     )
-)
 
 gas_rate_fig.update_layout(
-    title=f"Historia de Producción de Gas del pozo: {selected_sigla}",
+    title="Historia de Producción de Gas",
     xaxis_title="Meses",
-    yaxis_title="Caudal de Gas (km3/d)"
+    yaxis_title="Caudal de Gas (km3/d)",
 )
-gas_rate_fig.update_yaxes(range=[0, None])
 st.plotly_chart(gas_rate_fig)
 
 # Plot oil rate using Plotly
 oil_rate_fig = go.Figure()
 
-oil_rate_fig.add_trace(
-    go.Scatter(
-        x=matching_data['counter'],
-        y=matching_data['oil_rate'],
-        mode='lines+markers',
-        name='Oil Rate',
-        line=dict(color='green')
+for sigla in selected_sigla:
+    filtered_well_data = filtered_data[filtered_data['sigla'] == sigla]
+    
+    # Filter data to start when 'Np' is different from zero
+    filtered_well_data = filtered_well_data[filtered_well_data['Np'] != 0]
+    
+    # Add a counter column to the filtered data
+    filtered_well_data['counter'] = range(1, len(filtered_well_data) + 1)
+    
+    oil_rate_fig.add_trace(
+        go.Scatter(
+            x=filtered_well_data['counter'],  # Use the counter as x-axis
+            y=filtered_well_data['oil_rate'],
+            mode='lines+markers',
+            name=f'Oil Rate - {sigla}',
+        )
     )
-)
 
 oil_rate_fig.update_layout(
-    title=f"Historia de Producción de Petróleo del pozo: {selected_sigla}",
+    title="Historia de Producción de Petróleo",
     xaxis_title="Meses",
-    yaxis_title="Caudal de Petróleo (m3/d)"
+    yaxis_title="Caudal de Petróleo (m3/d)",
 )
-oil_rate_fig.update_yaxes(rangemode='tozero')
 st.plotly_chart(oil_rate_fig)
 
 # Plot water rate using Plotly
 water_rate_fig = go.Figure()
 
-water_rate_fig.add_trace(
-    go.Scatter(
-        x=matching_data['counter'],
-        y=matching_data['water_rate'],
-        mode='lines+markers',
-        name='Water Rate',
-        line=dict(color='blue')
+for sigla in selected_sigla:
+    filtered_well_data = filtered_data[filtered_data['sigla'] == sigla]
+    
+    # Filter data to start when 'Np' is different from zero
+    filtered_well_data = filtered_well_data[filtered_well_data['Np'] != 0]
+    
+    # Add a counter column to the filtered data
+    filtered_well_data['counter'] = range(1, len(filtered_well_data) + 1)
+    
+    water_rate_fig.add_trace(
+        go.Scatter(
+            x=filtered_well_data['counter'],  # Use the counter as x-axis
+            y=filtered_well_data['water_rate'],
+            mode='lines+markers',
+            name=f'Water Rate - {sigla}',
+        )
     )
-)
 
 water_rate_fig.update_layout(
-    title=f"Historia de Producción de Agua del pozo: {selected_sigla}",
+    title="Historia de Producción de Agua",
     xaxis_title="Meses",
-    yaxis_title="Caudal de Agua (m3/d)"
+    yaxis_title="Caudal de Agua (m3/d)",
 )
-water_rate_fig.update_yaxes(range=[0, None])
 st.plotly_chart(water_rate_fig)
-
-
