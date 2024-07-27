4e4f1794d6b464e9d2d55669a68794a6d8e30f29
Agustina <142050237+aggg97@users.noreply.github.com>
2024-07-27 19:37:12 +0200

    Update 4_👩‍🔧_FracData.py

--- pages/4_👩‍🔧_FracData.py
+++ pages/4_👩‍🔧_FracData.py
@@ -1,3 +1,19 @@
 import streamlit as st
+import pandas as pd
+import plotly.graph_objects as go
+import plotly.express as px
+from PIL import Image
 
-st.caption("Coming soon...")
+# Load and preprocess the data
+@st.cache_data
+def load_and_sort_data(dataset_url):
+    df = pd.read_csv(dataset_url)
+    return df
+
+# URL of the dataset
+dataset_url = "https://datos.gob.ar/dataset/energia-datos-fractura-pozos-hidrocarburos-adjunto-iv/archivo/energia_2280ad92-6ed3-403e-a095-50139863ab0d.csv"
+
+# Load the data
+data_sorted = load_and_sort_data(dataset_url)
+
+
