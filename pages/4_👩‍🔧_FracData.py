fe29190d5c093da19b54cebd34069209889c3f77
Agustina <142050237+aggg97@users.noreply.github.com>
2024-07-28 14:25:27 +0200

    Update 4_👩‍🔧_FracData.py

--- pages/4_👩‍🔧_FracData.py
+++ pages/4_👩‍🔧_FracData.py
@@ -6,7 +6,7 @@ from PIL import Image
 
 st.caption("Coming soon...")
 
-COLUMNS = [
+COLUMNS_FRAC= [
     'sigla',  # atemporal
     'longitud_rama_horizontal_m',
     'cantidad_fracturas',
@@ -18,16 +18,16 @@ COLUMNS = [
 # Load and preprocess the data
 @st.cache_data
 def load_and_sort_data(dataset_url):
-    df = pd.read_csv(dataset_url, usecols=COLUMNS)
-    return df
+    df_frac = pd.read_csv(dataset_url, usecols=COLUMNS_FRAC)
+    return df_frac
 
 # URL of the dataset
 dataset_url = "http://datos.energia.gob.ar/dataset/71fa2e84-0316-4a1b-af68-7f35e41f58d7/resource/2280ad92-6ed3-403e-a095-50139863ab0d/download/datos-de-fractura-de-pozos-de-hidrocarburos-adjunto-iv-actualizacin-diaria.csv"
 
 # Load the data
-df = load_and_sort_data(dataset_url)
+df_frac= load_and_sort_data(dataset_url)
 
 # Display the first few rows of the dataframe in Streamlit
 st.write("### Data Preview")
-st.write(df.head())
+st.write(df_frac.head())
 
