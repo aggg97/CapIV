c9f25d03ebc36c9422cc1761b4ba0037cf636bd4
Agustina <142050237+aggg97@users.noreply.github.com>
2024-07-28 14:25:47 +0200

    Update 4_👩‍🔧_FracData.py

--- pages/4_👩‍🔧_FracData.py
+++ pages/4_👩‍🔧_FracData.py
@@ -22,10 +22,10 @@ def load_and_sort_data(dataset_url):
     return df_frac
 
 # URL of the dataset
-dataset_url = "http://datos.energia.gob.ar/dataset/71fa2e84-0316-4a1b-af68-7f35e41f58d7/resource/2280ad92-6ed3-403e-a095-50139863ab0d/download/datos-de-fractura-de-pozos-de-hidrocarburos-adjunto-iv-actualizacin-diaria.csv"
+dataset_frac_url = "http://datos.energia.gob.ar/dataset/71fa2e84-0316-4a1b-af68-7f35e41f58d7/resource/2280ad92-6ed3-403e-a095-50139863ab0d/download/datos-de-fractura-de-pozos-de-hidrocarburos-adjunto-iv-actualizacin-diaria.csv"
 
 # Load the data
-df_frac= load_and_sort_data(dataset_url)
+df_frac= load_and_sort_data(dataset_frac_url)
 
 # Display the first few rows of the dataframe in Streamlit
 st.write("### Data Preview")
