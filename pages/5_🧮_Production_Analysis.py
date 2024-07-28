6c80ee17cef42f59321f7806dd40a20aa1a5bec6
Agustina <142050237+aggg97@users.noreply.github.com>
2024-07-28 02:10:38 +0200

    Update 5_🧮_Production_Analysis.py

--- pages/5_🧮_Production_Analysis.py
+++ pages/5_🧮_Production_Analysis.py
@@ -6,20 +6,20 @@ import plotly.express as px
 
 # Define the columns for the dataset
 COLUMNS = [
-    'sigla',  # atemporal
-    'anio',  # temporal
-    'mes',  # temporal
-    'prod_pet',  # temporal
-    'prod_gas',  # temporal
-    'prod_agua',  # temporal
-    'iny_gas',  # temporal
-    'tef',  # temporal
-    'tipoextraccion',  # atemporal
-    'tipopozo',  # atemporal
-    'empresa',  # atemporal
-    'formacion',  # atemporal
-    'areayacimiento',  # atemporal
-    'fecha_data'  # temporal
+    'sigla',  # well identifier
+    'anio',  # year
+    'mes',  # month
+    'prod_pet',  # oil production
+    'prod_gas',  # gas production
+    'prod_agua',  # water production
+    'iny_gas',  # gas injection
+    'tef',  # effective time
+    'tipoextraccion',  # extraction type
+    'tipopozo',  # well type
+    'empresa',  # company
+    'formacion',  # formation
+    'areayacimiento',  # field area
+    'fecha_data'  # data date
 ]
 
 # Load and sort the data
@@ -44,44 +44,34 @@ image = Image.open('Vaca Muerta rig.png')
 st.sidebar.image(image)
 st.sidebar.title("Por favor filtrar aquí:")
 
-# Create a multiselect menu for companies
-selected_companies = st.sidebar.multiselect(
-    "Seleccione las empresas",
-    options=data_sorted['empresa'].unique(),
-    default=data_sorted['empresa'].unique()
+# Selectbox for companies
+selected_company = st.sidebar.selectbox(
+    "Seleccione la empresa",
+    options=data_sorted['empresa'].unique()
 )
 
-# Create a dropdown menu for year
-selected_year = st.sidebar.selectbox(
-    "Seleccione el año",
-    options=data_sorted['anio'].unique()
-)
+# Filter data based on selected company
+company_data = data_sorted[data_sorted['empresa'] == selected_company]
 
-# Filter data based on the selected companies and year
-filtered_data = data_sorted[
-    (data_sorted['empresa'].isin(selected_companies)) & 
-    (data_sorted['anio'] == selected_year)
-]
-
-# Summarize production data by company
-summary_df = filtered_data.groupby(['empresa', 'date']).agg(
+# Summarize production data by field area
+summary_df = company_data.groupby(['areayacimiento', 'date']).agg(
     total_gas_rate=('gas_rate', 'sum'),
     total_oil_rate=('oil_rate', 'sum')
 ).reset_index()
 
-# Plot total oil production by company over time using area plot
+# Plot total oil production by field area over time using area plot
 oil_rate_fig = go.Figure()
 
 color_palette = px.colors.qualitative.Set3  # Use a distinct color palette
 
-for i, company in enumerate(summary_df['empresa'].unique()):
-    company_data = summary_df[summary_df['empresa'] == company]
+for i, area in enumerate(summary_df['areayacimiento'].unique()):
+    area_data = summary_df[summary_df['areayacimiento'] == area]
     oil_rate_fig.add_trace(
         go.Scatter(
-            x=company_data['date'],
-            y=company_data['total_oil_rate'],
+            x=area_data['date'],
+            y=area_data['total_oil_rate'],
             mode='lines',
-            name=f'{company} - Oil Rate',
+            name=f'{area} - Oil Rate',
             fill='tonexty',
             line=dict(color=color_palette[i % len(color_palette)]),
             hovertemplate='Fecha: %{x}<br>Caudal de Petróleo: %{y:.2f} m3/d'
@@ -89,27 +79,27 @@ for i, company in enumerate(summary_df['empresa'].unique()):
     )
 
 oil_rate_fig.update_layout(
-    title="Producción Total de Petróleo por Empresa",
+    title="Producción Total de Petróleo por Área de Yacimiento",
     xaxis_title="Fecha",
     yaxis_title="Caudal de Petróleo (m3/d)",
     hovermode='x unified',
-    legend_title="Empresa"
+    legend_title="Área de Yacimiento"
 )
 
 # Display the oil production plot
 st.plotly_chart(oil_rate_fig, use_container_width=True)
 
-# Plot total gas production by company over time using area plot
+# Plot total gas production by field area over time using area plot
 gas_rate_fig = go.Figure()
 
-for i, company in enumerate(summary_df['empresa'].unique()):
-    company_data = summary_df[summary_df['empresa'] == company]
+for i, area in enumerate(summary_df['areayacimiento'].unique()):
+    area_data = summary_df[summary_df['areayacimiento'] == area]
     gas_rate_fig.add_trace(
         go.Scatter(
-            x=company_data['date'],
-            y=company_data['total_gas_rate'],
+            x=area_data['date'],
+            y=area_data['total_gas_rate'],
             mode='lines',
-            name=f'{company} - Gas Rate',
+            name=f'{area} - Gas Rate',
             fill='tonexty',
             line=dict(color=color_palette[i % len(color_palette)]),
             hovertemplate='Fecha: %{x}<br>Caudal de Gas: %{y:.2f} km3/d'
@@ -117,52 +107,37 @@ for i, company in enumerate(summary_df['empresa'].unique()):
     )
 
 gas_rate_fig.update_layout(
-    title="Producción Total de Gas por Empresa",
+    title="Producción Total de Gas por Área de Yacimiento",
     xaxis_title="Fecha",
     yaxis_title="Caudal de Gas (km3/d)",
     hovermode='x unified',
-    legend_title="Empresa"
+    legend_title="Área de Yacimiento"
 )
 
 # Display the gas production plot
 st.plotly_chart(gas_rate_fig, use_container_width=True)
 
-# Identify top 10 wells for gas and oil based on the maximum production rates for the selected companies and year
-top_10_gas_wells = filtered_data.sort_values(by='gas_rate', ascending=False).head(10)['sigla'].unique()
-top_10_oil_wells = filtered_data.sort_values(by='oil_rate', ascending=False).head(10)['sigla'].unique()
-
-# Plot top 10 wells production profile for gas
-top_gas_fig = go.Figure()
-
-for i, well in enumerate(top_10_gas_wells):
-    well_data = filtered_data[filtered_data['sigla'] == well]
-    top_gas_fig.add_trace(
-        go.Scatter(
-            x=well_data['date'],
-            y=well_data['gas_rate'],
-            mode='lines+markers',
-            name=f'{well} - Gas Rate',
-            line=dict(color=color_palette[i % len(color_palette)]),
-            hovertemplate='Fecha: %{x}<br>Caudal de Gas: %{y:.2f} km3/d'
-        )
-    )
-
-top_gas_fig.update_layout(
-    title=f"Top 10 Pozos por Perfil de Producción de Gas en {selected_year}",
-    xaxis_title="Fecha",
-    yaxis_title="Caudal de Gas (km3/d)",
-    hovermode='x unified',
-    legend_title="Pozos"
+# Selectbox for areas based on selected company
+selected_area = st.sidebar.selectbox(
+    "Seleccione el área de yacimiento",
+    options=company_data['areayacimiento'].unique()
 )
 
-# Display the top 10 wells gas production plot
-st.plotly_chart(top_gas_fig, use_container_width=True)
+# Number input for year selection
+selected_year = st.sidebar.number_input('Ingrese el año', min_value=int(data_sorted['anio'].min()), max_value=int(data_sorted['anio'].max()), value=int(data_sorted['anio'].max()), step=1)
+
+# Filter data based on selected area and year
+area_year_data = company_data[(company_data['areayacimiento'] == selected_area) & (company_data['anio'] == selected_year)]
+
+# Identify top 10 wells for oil and gas based on the highest production rates in the selected year
+top_10_oil_wells = area_year_data.sort_values(by='oil_rate', ascending=False).head(10)['sigla'].unique()
+top_10_gas_wells = area_year_data.sort_values(by='gas_rate', ascending=False).head(10)['sigla'].unique()
 
 # Plot top 10 wells production profile for oil
 top_oil_fig = go.Figure()
 
 for i, well in enumerate(top_10_oil_wells):
-    well_data = filtered_data[filtered_data['sigla'] == well]
+    well_data = area_year_data[area_year_data['sigla'] == well]
     top_oil_fig.add_trace(
         go.Scatter(
             x=well_data['date'],
@@ -185,63 +160,29 @@ top_oil_fig.update_layout(
 # Display the top 10 wells oil production plot
 st.plotly_chart(top_oil_fig, use_container_width=True)
 
-# Option to download the filtered data
-csv = filtered_data.to_csv(index=False)
-st.sidebar.download_button(
-    label="Descargar datos filtrados",
-    data=csv,
-    file_name='filtered_data.csv',
-    mime='text/csv',
+# Plot top 10 wells production profile for gas
+top_gas_fig = go.Figure()
+
+for i, well in enumerate(top_10_gas_wells):
+    well_data = area_year_data[area_year_data['sigla'] == well]
+    top_gas_fig.add_trace(
+        go.Scatter(
+            x=well_data['date'],
+            y=well_data['gas_rate'],
+            mode='lines+markers',
+            name=f'{well} - Gas Rate',
+            line=dict(color=color_palette[i % len(color_palette)]),
+            hovertemplate='Fecha: %{x}<br>Caudal de Gas: %{y:.2f} km3/d'
+        )
+    )
+
+top_gas_fig.update_layout(
+    title=f"Top 10 Pozos por Perfil de Producción de Gas en {selected_year}",
+    xaxis_title="Fecha",
+    yaxis_title="Caudal de Gas (km3/d)",
+    hovermode='x unified',
+    legend_title="Pozos"
 )
 
-# Optionally add plot smoothing
-st.sidebar.title("Opciones de Suavizado")
-smoothing = st.sidebar.checkbox("Activar suavizado de la curva")
-if smoothing:
-    from scipy.ndimage import gaussian_filter1d
-    
-    oil_rate_fig_smooth = go.Figure()
-    gas_rate_fig_smooth = go.Figure()
-    
-    for i, company in enumerate(summary_df['empresa'].unique()):
-        company_data = summary_df[summary_df['empresa'] == company]
-        oil_rate_fig_smooth.add_trace(
-            go.Scatter(
-                x=company_data['date'],
-                y=gaussian_filter1d(company_data['total_oil_rate'], sigma=2),
-                mode='lines',
-                name=f'{company} - Oil Rate (Suavizado)',
-                fill='tonexty',
-                line=dict(color=color_palette[i % len(color_palette)]),
-                hovertemplate='Fecha: %{x}<br>Caudal de Petróleo: %{y:.2f} m3/d'
-            )
-        )
-        gas_rate_fig_smooth.add_trace(
-            go.Scatter(
-                x=company_data['date'],
-                y=gaussian_filter1d(company_data['total_gas_rate'], sigma=2),
-                mode='lines',
-                name=f'{company} - Gas Rate (Suavizado)',
-                fill='tonexty',
-                line=dict(color=color_palette[i % len(color_palette)]),
-                hovertemplate='Fecha: %{x}<br>Caudal de Gas: %{y:.2f} km3/d'
-            )
-        )
-    
-    oil_rate_fig_smooth.update_layout(
-        title="Producción Total de Petróleo por Empresa (Suavizado)",
-        xaxis_title="Fecha",
-        yaxis_title="Caudal de Petróleo (m3/d)",
-        hovermode='x unified',
-        legend_title="Empresa"
-    )
-    gas_rate_fig_smooth.update_layout(
-        title="Producción Total de Gas por Empresa (Suavizado)",
-        xaxis_title="Fecha",
-        yaxis_title="Caudal de Gas (km3/d)",
-        hovermode='x unified',
-        legend_title="Empresa"
-    )
-    
-    st.plotly_chart(oil_rate_fig_smooth, use_container_width=True)
-    st.plotly_chart(gas_rate_fig_smooth, use_container_width=True)
+# Display the top 10 wells gas production plot
+st.plotly_chart(top_gas_fig, use_container_width=True)
