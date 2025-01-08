a8f54bdb307902f2032bd326955757d5b2de55a2
Agustina <142050237+aggg97@users.noreply.github.com>
2025-01-08 19:00:49 +0100

    Update 6_👩‍🔧_FracData.py

--- pages/6_👩‍🔧_FracData.py
+++ pages/6_👩‍🔧_FracData.py
@@ -47,7 +47,7 @@ data_sorted['empresaNEW'] = data_sorted['empresa'].replace(replacement_dict)
 
 # Sidebar filters
 st.header(f":blue[Análisis de Producción No Convencional]")
-image = Image.open('Vaca Muerta rig.png')
+image = Image.open('rig.webp')
 st.sidebar.image(image)
 st.sidebar.title("Por favor filtrar aquí:")
 
