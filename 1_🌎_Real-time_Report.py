e1beda901a8dcef139c9c77460d21ab719a98b9a
Agustina <142050237+aggg97@users.noreply.github.com>
2025-01-08 20:01:06 +0100

    Update 1_🌎_Real-time_Report.py

--- 1_🌎_Real-time_Report.py
+++ 1_🌎_Real-time_Report.py
@@ -1,37 +1,72 @@
-import streamlit as st
 import pandas as pd
 import plotly.express as px
+import plotly.graph_objects as go
+import streamlit as st
 from PIL import Image
 
-# Load and preprocess the data
+# Load and preprocess the production data
 @st.cache_data
 def load_and_sort_data(dataset_url):
     try:
         df = pd.read_csv(dataset_url, usecols=[
-            'sigla', 'anio', 'mes', 'prod_pet', 'prod_gas', 'tef', 'empresa', 'areayacimiento', 'fecha_data'
+            'sigla', 'anio', 'mes', 'prod_pet', 'prod_gas', 'prod_agua',
+            'tef', 'empresa', 'areayacimiento', 'coordenadax', 'coordenaday',
+            'formprod', 'sub_tipo_recurso', 'tipopozo'
         ])
         df['date'] = pd.to_datetime(df['anio'].astype(str) + '-' + df['mes'].astype(str) + '-1')
         df['gas_rate'] = df['prod_gas'] / df['tef']
         df['oil_rate'] = df['prod_pet'] / df['tef']
+        df['water_rate'] = df['prod_agua'] / df['tef']
+        df['Np'] = df.groupby('sigla')['prod_pet'].cumsum()
+        df['Gp'] = df.groupby('sigla')['prod_gas'].cumsum()
+        df['Wp'] = df.groupby('sigla')['prod_agua'].cumsum()
         return df
     except Exception as e:
         st.error(f"Error loading data: {e}")
-        return pd.DataFrame()  # Return an empty DataFrame in case of error
+        return pd.DataFrame()
 
-# URL of the dataset
+# URLs for datasets
 dataset_url = "http://datos.energia.gob.ar/dataset/c846e79c-026c-4040-897f-1ad3543b407c/resource/b5b58cdc-9e07-41f9-b392-fb9ec68b0725/download/produccin-de-pozos-de-gas-y-petrleo-no-convencional.csv"
 
-# Load the data
+# Load the production data
 data_sorted = load_and_sort_data(dataset_url)
 
 if data_sorted.empty:
-    st.stop()  # Stop execution if data failed to load
+    st.error("Failed to load production data.")
+    st.stop()
 
-# Filter out rows where tef is zero for calculating metrics
-data_filtered = data_sorted[data_sorted['tef'] > 0]
+# Replace company names in production data
+replacement_dict = {
+    'PAN AMERICAN ENERGY (SUCURSAL ARGENTINA) LLC': 'PAN AMERICAN ENERGY',
+    'PAN AMERICAN ENERGY SL': 'PAN AMERICAN ENERGY',
+    'VISTA ENERGY ARGENTINA SAU': 'VISTA',
+    'Vista Oil & Gas Argentina SA': 'VISTA',
+    'VISTA OIL & GAS ARGENTINA SAU': 'VISTA',
+    'WINTERSHALL DE ARGENTINA S.A.': 'WINTERSHALL',
+    'WINTERSHALL ENERGÍA S.A.': 'WINTERSHALL'
+}
+data_sorted['empresaNEW'] = data_sorted['empresa'].replace(replacement_dict)
+
+# Sidebar filters
+st.header(f":blue[Reporte de Producción No Convencional]")
+image = Image.open('Vaca Muerta rig.png')
+st.sidebar.image(image)
+
+# Filter out rows where TEF is zero for calculating metrics
+data_filtered = data_sorted[(data_sorted['tef'] > 0)]
 
 # Find the latest date in the dataset
 latest_date = data_filtered['date'].max()
+
+from dateutil.relativedelta import relativedelta
+
+# Find the latest date in the dataset
+latest_date_non_official = data_filtered['date'].max()
+
+# Subtract 1 month from the latest date
+latest_date = latest_date_non_official - relativedelta(months=1)
+
+print(latest_date)
 
 # Filter the dataset to include only rows from the latest date
 latest_data = data_filtered[data_filtered['date'] == latest_date]
@@ -48,31 +83,33 @@ total_gas_rate_rounded = round(total_gas_rate, 1)
 total_oil_rate_rounded = round(total_oil_rate, 1)
 oil_rate_bpd_rounded = round(oil_rate_bpd, 1)
 
-# Load and preprocess data for plotting
-company_summary = data_filtered.groupby(['empresa', 'date']).agg(
+print(total_gas_rate_rounded,total_oil_rate_rounded,oil_rate_bpd_rounded)
+
+# Group and aggregate data for plotting
+company_summary = data_filtered.groupby(['empresaNEW', 'date']).agg(
     total_gas_rate=('gas_rate', 'sum'),
     total_oil_rate=('oil_rate', 'sum')
 ).reset_index()
 
 # Determine top 10 companies by total oil production
-top_companies = company_summary.groupby('empresa')['total_oil_rate'].sum().nlargest(10).index
+top_companies = company_summary.groupby('empresaNEW')['total_oil_rate'].sum().nlargest(10).index
 
 # Aggregate data for top companies and "Others"
-company_summary['empresa'] = company_summary['empresa'].apply(lambda x: x if x in top_companies else 'Others')
-company_summary_aggregated = company_summary.groupby(['empresa', 'date']).agg(
+company_summary['empresaNEW'] = company_summary['empresaNEW'].apply(lambda x: x if x in top_companies else 'Otros')
+company_summary_aggregated = company_summary.groupby(['empresaNEW', 'date']).agg(
     total_gas_rate=('total_gas_rate', 'sum'),
     total_oil_rate=('total_oil_rate', 'sum')
 ).reset_index()
 
 # Count wells per company
-well_count = data_filtered.groupby('empresa')['sigla'].nunique().reset_index()
-well_count.columns = ['empresa', 'well_count']
+well_count = data_filtered.groupby('empresaNEW')['sigla'].nunique().reset_index()
+well_count.columns = ['empresaNEW', 'well_count']
 
 # Determine top 10 companies by number of wells
-top_wells_companies = well_count.nlargest(10, 'well_count')['empresa']
+top_wells_companies = well_count.nlargest(10, 'well_count')['empresaNEW']
 
 # Filter well_count to include only top companies
-well_count_top = well_count[well_count['empresa'].isin(top_wells_companies)]
+well_count_top = well_count[well_count['empresaNEW'].isin(top_wells_companies)]
 
 # Determine the starting year for each well
 well_start_year = data_filtered.groupby('sigla')['anio'].min().reset_index()
@@ -90,112 +127,111 @@ yearly_summary = data_with_start_year.groupby(['start_year', 'date']).agg(
 # Filter out rows where cumulative gas and oil production are zero or less
 yearly_summary = yearly_summary[(yearly_summary['total_gas_rate'] > 0) & (yearly_summary['total_oil_rate'] > 0)]
 
-# Create Streamlit app layout
-st.header(":blue[Reporte de Producción No Convencional]")
-
-image = Image.open('Vaca Muerta rig.png')
-st.sidebar.image(image)
-st.sidebar.title("Por favor filtrar aquí:")
-
+st.write("Fecha de Última Alocación Finalizada: ", latest_date)
 # Display total gas rate and oil rate metrics
 col1, col2, col3 = st.columns(3)
 col1.metric(label=":red[Total Caudal de Gas (MMm³/d)]", value=total_gas_rate_rounded)
 col2.metric(label=":green[Total Caudal de Petróleo (km³/d)]", value=total_oil_rate_rounded)
 col3.metric(label=":green[Total Caudal de Petróleo (kbpd)]", value=oil_rate_bpd_rounded)
 
-# Area plots for gas and oil rates by top 10 companies + Others
-st.subheader("Actividad de las principales empresas")
+# ------------------------ PLOTS ------------------------
 
-# Plot for gas rate by company
-fig_gas_company = px.area(company_summary_aggregated, x='date', y='total_gas_rate', color='empresa', title="Caudal de Gas por Empresa")
+import plotly.express as px
+import plotly.graph_objects as go
+import streamlit as st
+
+# Plot gas rate by company
+fig_gas_company = px.area(
+    company_summary_aggregated, 
+    x='date', y='total_gas_rate', color='empresaNEW', 
+    title="Caudal de Gas por Empresa"
+)
 fig_gas_company.update_layout(
-    legend_title_text='Empresa',
-    legend=dict(
-        orientation="h",
-        yanchor="top",
-        y=-0.3,  # Adjust this value to avoid overlapping
-        xanchor="center",
-        x=0.5,
-        font=dict(size=10)  # Adjust the font size to fit the space
-    ),
-    margin=dict(b=100),  # Increase the bottom margin to make space for the legend
     xaxis_title="Fecha",
-    yaxis_title="Caudal de Gas (km³/d)"
+    yaxis_title="Caudal de Gas (km³/d)",
+    legend_title="Empresa",
+    legend=dict(
+        orientation="h",  # Horizontal legend
+        yanchor="top",  # Position the legend at the bottom
+        y=-0.4,  # Position the legend further below the plot area
+        xanchor="center",  # Center the legend horizontally
+        x=0.5,  # Center the legend horizontally
+        font=dict(size=10)  # Adjust font size to fit space
+    ),
+    margin=dict(b=200),  # Increase bottom margin to accommodate the legend without overlap
+    xaxis=dict(tickangle=45),  # Rotate x-axis labels to prevent overlap
+    xaxis_title_standoff=20  # Add more space between x-axis and plot content
 )
-st.plotly_chart(fig_gas_company, use_container_width=True)
 
-# Plot for oil rate by company
-fig_oil_company = px.area(company_summary_aggregated, x='date', y='total_oil_rate', color='empresa', title="Caudal de Petróleo por Empresa")
+# Plot oil rate by company
+fig_oil_company = px.area(
+    company_summary_aggregated, 
+    x='date', y='total_oil_rate', color='empresaNEW', 
+    title="Caudal de Petróleo por Empresa"
+)
 fig_oil_company.update_layout(
-    legend_title_text='Empresa',
-    legend=dict(
-        orientation="h",
-        yanchor="top",
-        y=-0.3,  # Adjust this value to avoid overlapping
-        xanchor="center",
-        x=0.5,
-        font=dict(size=10)  # Adjust the font size to fit the space
-    ),
-    margin=dict(b=100),  # Increase the bottom margin to make space for the legend
     xaxis_title="Fecha",
-    yaxis_title="Caudal de Petróleo (m³/d)"
+    yaxis_title="Caudal de Petróleo (m³/d)",
+    legend_title="Empresa",
+    legend=dict(
+        orientation="h",  # Horizontal legend
+        yanchor="top",  # Position the legend at the bottom
+        y=-0.4,  # Position the legend further below the plot area
+        xanchor="center",  # Center the legend horizontally
+        x=0.5,  # Center the legend horizontally
+        font=dict(size=10)  # Adjust font size to fit space
+    ),
+    margin=dict(b=200),  # Increase bottom margin to accommodate the legend without overlap
+    xaxis=dict(tickangle=45),  # Rotate x-axis labels to prevent overlap
+    xaxis_title_standoff=20  # Add more space between x-axis and plot content
 )
-st.plotly_chart(fig_oil_company, use_container_width=True)
-
-# Treemap of the number of wells per company
-st.subheader("Número de Pozos por Empresa")
-
-fig_wells = px.treemap(well_count, path=['empresa'], values='well_count', title="Número de Pozos por Empresa")
-fig_wells.update_layout(
-    treemapcolorway=px.colors.qualitative.Pastel,
-    margin=dict(t=50, l=25, r=25, b=25)
-)
-st.plotly_chart(fig_wells, use_container_width=True)
-
-# Area plots for gas and oil rates by well start year
-st.subheader("Producción por Año de Inicio de Pozo")
 
 # Plot for gas rate by start year
-fig_gas_year = px.area(yearly_summary, x='date', y='total_gas_rate', color='start_year', title="Caudal de Gas por Año de Inicio de Pozo")
-fig_gas_year.update_layout(
-    legend_title_text='Año de Inicio de Pozo',
-    legend=dict(
-        orientation="h",
-        yanchor="top",
-        y=-0.3,  # Adjust this value to avoid overlapping
-        xanchor="center",
-        x=0.5,
-        font=dict(size=10)  # Adjust the font size to fit the space
-    ),
-    margin=dict(b=100),  # Increase the bottom margin to make space for the legend
-    xaxis_title="Fecha",
-    yaxis_title="Caudal de Gas (km³/d)"
+fig_gas_year = px.area(
+    yearly_summary, 
+    x='date', y='total_gas_rate', color='start_year', 
+    title="Caudal de Gas por Año de Puesta en Marcha de Pozo"
 )
-st.plotly_chart(fig_gas_year, use_container_width=True)
+fig_gas_year.update_layout(
+    legend_title="Año de Puesta en Marcha de Pozo",
+    legend=dict(
+        orientation="h",  # Horizontal legend
+        yanchor="top",  # Position the legend at the bottom
+        y=-0.4,  # Position the legend further below the plot area
+        xanchor="center",  # Center the legend horizontally
+        x=0.5,  # Center the legend horizontally
+        font=dict(size=10)  # Adjust font size to fit space
+    ),
+    margin=dict(b=200),  # Increase bottom margin to accommodate the legend without overlap
+    xaxis_title="Fecha",
+    yaxis_title="Caudal de Gas (km³/d)",
+    xaxis_title_standoff=20  # Add more space between x-axis and plot content
+)
 
 # Plot for oil rate by start year
-fig_oil_year = px.area(yearly_summary, x='date', y='total_oil_rate', color='start_year', title="Caudal de Petróleo por Año de Inicio de Pozo")
+fig_oil_year = px.area(
+    yearly_summary, 
+    x='date', y='total_oil_rate', color='start_year', 
+    title="Caudal de Petróleo por Año de Puesta en Marcha de Pozo"
+)
 fig_oil_year.update_layout(
-    legend_title_text='Año de Inicio de Pozo',
+    legend_title="Año de Puesta en Marcha de Pozo",
     legend=dict(
-        orientation="h",
-        yanchor="top",
-        y=-0.3,  # Adjust this value to avoid overlapping
-        xanchor="center",
-        x=0.5,
-        font=dict(size=10)  # Adjust the font size to fit the space
+        orientation="h",  # Horizontal legend
+        yanchor="top",  # Position the legend at the bottom
+        y=-0.4,  # Position the legend further below the plot area
+        xanchor="center",  # Center the legend horizontally
+        x=0.5,  # Center the legend horizontally
+        font=dict(size=10)  # Adjust font size to fit space
     ),
-    margin=dict(b=100),  # Increase the bottom margin to make space for the legend
+    margin=dict(b=200),  # Increase bottom margin to accommodate the legend without overlap
     xaxis_title="Fecha",
-    yaxis_title="Caudal de Petróleo (m³/d)"
+    yaxis_title="Caudal de Petróleo (m³/d)",
+    xaxis_title_standoff=20  # Add more space between x-axis and plot content
 )
-st.plotly_chart(fig_oil_year, use_container_width=True)
 
-# Option to download the filtered data
-csv = data_filtered.to_csv(index=False)
-st.sidebar.download_button(
-    label="Descargar datos",
-    data=csv,
-    file_name='filtered_data.csv',
-    mime='text/csv',
-)
+# Plot the charts
+st.plotly_chart(fig_gas_company)
+st.plotly_chart(fig_oil_company)
+st.plotly_chart(fig_gas_year)
+st.plotly_chart(fig_oil_year)
