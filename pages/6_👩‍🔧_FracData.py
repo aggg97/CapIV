f2b0155e654ff5c2114c72596499656322a1a11c
Agustina <142050237+aggg97@users.noreply.github.com>
2025-01-09 14:31:13 +0100

    Update 6_👩‍🔧_FracData.py

--- pages/6_👩‍🔧_FracData.py
+++ pages/6_👩‍🔧_FracData.py
@@ -469,7 +469,7 @@ fig.update_layout(
 
 # Show the plot
 fig.show()
-st.plotly_chart(fig_rama, use_container_width=True)
+st.plotly_chart(fig, use_container_width=True)
 
 # -----------------------
 
